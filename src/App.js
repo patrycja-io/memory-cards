370b55e0c53a303fd799128c4e94ec43806cd52c
Patrycja <patrycja@dynium.ai>
2022-04-27 15:05:37 +0100

    added function to map cards array

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import './App.css'
+import {useState} from 'react'
+
+const images=[
+  {'src': "assets/astronaut.png"},
+  {'src': "assets/ball.png"},
+  {'src': "assets/camera.png"},
+  {'src': "assets/rocket.png"},
+  {'src': "assets/hotdog.png"},
+  {'src': "assets/key.png"},
+  {'src': "assets/planet.png"},
+  {'src': "assets/victory.png"}
+
+]
 
 function App() {
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+
+  // mixmach cards
+
+  const randomCards =() =>{
+    const changeCards =[...images, ...images]
+    .sort(() => Math.random() -0.5)
+    .map((card) => ({...card, id: Math.random()}))
+
+    setCards(changeCards)
+    setTurns(0)
+  }
   return (
     <div className="App">
       <h1>Memory Cards</h1>
-      <button>Play New Game</button>
+      <button onClick={randomCards}>Play New Game</button>
     </div>
   );
 }
