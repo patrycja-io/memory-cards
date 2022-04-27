14c8bdf4ae73ecef3478aa73fb50204f391411f7
Patrycja <patrycja@dynium.ai>
2022-04-27 14:09:58 +0100

    Initial Part

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Memory Cards</h1>
+      <button>Play New Game</button>
+    </div>
+  );
+}
+
+export default App
